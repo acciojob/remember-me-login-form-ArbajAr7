e1aa731df8c24932d2f57dd562157adccf3d291f
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-06-19 00:02:33 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+function clicked(){
+    let checkboxvalue = document.getElementById("checkbox").checked;
+    if(checkboxvalue == true)
+        {
+            let name = document.getElementById("username").value;
+            localStorage.setItem("name", name);
+            let password = document.getElementById("password").value;
+            localStorage.setItem("password", password);
+
+        }
+        Alert();
+}
+
+function Alert() {
+    alert("Logged in as .");
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    let name = localStorage.getItem("name");
+    let password = localStorage.getItem("password");
+    if(name != null && password != null)
+        {
+            document.getElementById("form").style.display = "none";
+            document.getElementById("existing").style.display = "block";
+            document.getElementById("existing").innerHTML =`Logged in as.`;
+
+        }else {
+            document.getElementById("form").style.display = "block";
+            document.getElementById("existing").style.display = "none";
+
+        }
+  });
