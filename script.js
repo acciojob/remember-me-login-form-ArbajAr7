c7ed0c0e77aeaaa30288a069128a9ff33271daf4
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-06-21 16:43:33 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
-//your JS code here. If required.
-function clicked(){
+function clicked(event){
+    event.preventDefault();
     let checkboxvalue = document.getElementById("checkbox").checked;
-    if(checkboxvalue == true)
-        {
-            let name = document.getElementById("username").value;
-            localStorage.setItem("name", name);
-            let password = document.getElementById("password").value;
-            localStorage.setItem("password", password);
-			Alert(name);
-
-        }else {
+    if(checkboxvalue == true) {
+        let name = document.getElementById("username").value;
+        localStorage.setItem("name", name);
+        let password = document.getElementById("password").value;
+        localStorage.setItem("password", password);
+    } else {
         localStorage.removeItem("name");
         localStorage.removeItem("password");
     }
+    Alert();
 }
 
-function Alert(name) {
-    alert("Logged in as "+name);
+function Alert() {
+    let name = localStorage.getItem("name");
+    if(name != null) {
+        alert("Logged in as "+name);
+    }
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
     let name = localStorage.getItem("name");
     let password = localStorage.getItem("password");
-    if(name != null && password != null)
-        {
-            document.getElementById("form").style.display = "none";
-            document.getElementById("existing").style.display = "block";
-            document.getElementById("existing").innerHTML =`Login as existing user`;
-			document.getElementById("username").value = name;
-	        document.getElementById("password").value = password;
-
-        }else {
-            document.getElementById("form").style.display = "block";
-            document.getElementById("existing").style.display = "none";
-
-        }
-  });
+    if(name != null && password != null) {
+        document.getElementById("existing").style.display = "block";
+        document.getElementById("username").value = name;
+        document.getElementById("password").value = password;
+    } else {
+        document.getElementById("existing").style.display = "none";
+    }
+});
